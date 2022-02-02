1e36ca64f7669e6ff49f1d3b4ca55d84eb62b9fb
Sarath C <sarathcs3c@gmail.com>
2022-02-02 17:30:14 +0530

    small design change

--- proj-movie/src/components/header/Header.js
+++ proj-movie/src/components/header/Header.js
@@ -2,6 +2,8 @@ import Navbar from "react-bootstrap/Navbar"
 import Container from "react-bootstrap/Container"
 import { NavLink } from 'react-router-dom'
 import './Header.css'
+import Button from "react-bootstrap/Button"
+// import { Button } from "@material-ui/core"
 
 const Header = () => {
 
@@ -11,9 +13,9 @@ const Header = () => {
                 <Container className="justify-content-center">
                     <Navbar.Text className='brandName' >PROJ-MOVIES</Navbar.Text>
                 </Container>
-                <Navbar.Brand>
-                    <NavLink to='/login'>LOGIN</NavLink>{ ' ' }
-                    <NavLink to='/register'>REGISTER</NavLink>
+                <Navbar.Brand >
+                    <Button  variant="info"><NavLink className='btns' to='/login'>LOGIN</NavLink></Button>{ ' ' }
+                    <Button variant="info"><NavLink className='btns' to='/register'>REGISTER</NavLink></Button>
                 </Navbar.Brand>
             </Navbar>
 
