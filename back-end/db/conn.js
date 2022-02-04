b692b6ef0e5068ff19487c6ce24ad735ba54b7ee
Sarath C <sarathcs3c@gmail.com>
2022-02-04 14:24:09 +0530

    changed from cloud database to local database

--- back-end/db/conn.js
+++ back-end/db/conn.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 require('dotenv').config({ path: './config/config.env' });
 
-const Db = process.env.ATLAS_URI
+const Db = process.env.LOCAL_URI
 const database = () => {
     mongoose.connect(Db, { useNewUrlParser: true,useUnifiedTopology: true }, (err) => {
         if (err) throw err;
+        console.log(err)
     })
-    console.log('Connected to Db')
+    console.log('Connected to database')
+
 }
 
 module.exports = database;
