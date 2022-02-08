d86fecfdb3e03c4cad3c9c2f208910b79d850541
Sarath C <sarathcs3c@gmail.com>
2022-02-08 17:28:39 +0530

    small change

--- proj-movie/src/components/search/Search.js
+++ proj-movie/src/components/search/Search.js
@@ -13,12 +13,14 @@ const Search = () => {
     const [searchData, setSearchData] = useState([])
 
     const getSearchData = () => {
-        axios.get(`https://api.tmdb.org/3/search/movie/?api_key=17e786d5aa65a489c613aaca6427cd5e&adult=false&query=${searchText}`).then(res => {
-            console.log(res.data.results)
-            setSearchData(res.data.results)
-        }).catch(e => {
-            console.log(e)
-        })
+        axios.get(`https://api.tmdb.org/3/search/movie/?api_key=17e786d5aa65a489c613aaca6427cd5e&adult=false&query=${searchText}`)
+            .then(res => {
+                console.log(res.data.results)
+                setSearchData(res.data.results)
+            })
+            .catch(e => {
+                console.log(e)
+            })
     }
     useEffect(() => {
         getSearchData()
@@ -61,22 +63,21 @@ const Search = () => {
                                             return (
                                                 <>
                                                     <Col>
-
-                                                    <Card style={{height: '450px'}} className="movieCard m-3">
-                                                        <Card.Img style={{height: '300px'}} variant="top" src={`https://image.tmdb.org/t/p/w300/${v.poster_path}`} />
-                                                        <Row>
-                                                            <Col>
-                                                                <Card.Text>{v.release_date }</Card.Text>
-                                                            </Col>
-                                                            <Col className='m-2'>
+                                                        <Card style={{height: '450px'}} className="movieCard m-3">
+                                                            <Card.Img style={{height: '300px'}} variant="top" src={`https://image.tmdb.org/t/p/w300/${v.poster_path}`} />
+                                                            <Row>
+                                                                <Col>
+                                                                    <Card.Text>{v.release_date }</Card.Text>
+                                                                </Col>
+                                                                <Col className='m-2'>
                                                                     <CircularProgress variant="determinate" value={v.vote_average * 10} />
                                                                     <Card.Text>{v.vote_average}</Card.Text>
-                                                            </Col>
-                                                        </Row>
-                                                        <Card.Body>
-                                                            <Card.Title>{v.title || v.name}</Card.Title>
-                                                        </Card.Body>
-                                                    </Card>
+                                                                </Col>
+                                                            </Row>
+                                                            <Card.Body>
+                                                                <Card.Title>{v.title || v.name}</Card.Title>
+                                                            </Card.Body>
+                                                        </Card>
                                                     </Col>
                                                 </>
                                             )
