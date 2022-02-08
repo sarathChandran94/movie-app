376612ca1593b89bd361a3b2ad2446d8a12b4421
Sarath C <sarathcs3c@gmail.com>
2022-02-08 17:23:53 +0530

    small error handling changes

--- back-end/db/conn.js
+++ back-end/db/conn.js
@@ -4,10 +4,14 @@ require('dotenv').config({ path: './config/config.env' });
 const Db = process.env.LOCAL_URI
 const database = () => {
     mongoose.connect(Db, { useNewUrlParser: true,useUnifiedTopology: true }, (err) => {
-        if (err) throw err;
-        console.log(err)
+        if (err) {
+            console.log(`error: database not connected`)
+            // throw err;
+        }
+        else if (!err) {
+            console.log('Connected to database')
+        }
     })
-    console.log('Connected to database')
 
 }
 
