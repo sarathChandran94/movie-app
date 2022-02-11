9e59438ec67c0ca330d9084d0336084162562863
Sarath C <sarathcs3c@gmail.com>
2022-02-11 17:12:51 +0530

    started adding password hash and token generation

--- back-end/routes/userRoute.js
+++ back-end/routes/userRoute.js
@@ -2,42 +2,55 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const user = require('../models/users');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.post('/newuser', (req, res) => {
     let userData = {}
+    var { name, email, password } = req.body;
 
-    user.findOne({ username: req.body.username }, (err, result) => {
-        if (result) {
+    user.findOne({ username: name }, (err, result) => {
+        if (err) {
+            res.send({msg: err.Error})
+        }
+        else if (result) {
             res.send({msg: 'Username taken!'})
-        } else {
-            if (req.body.username === 'admin') {
+        }
+        else if (!err) {
+            const salt = bcrypt.genSalt(10)
+            const hashedPassword = bcrypt.hash(password, salt)
+            if (name === 'admin') {
                 userData = {
-                    username: req.body.username,
-                    email: req.body.email,
-                    password: req.body.password,
+                    username: name,
+                    email: email,
+                    password: hashedPassword,
                     role: 'Admin'
                 }
-            } else {
+            }
+            else {
                 userData = {
-                    username: req.body.username,
-                    email: req.body.email,
-                    password: req.body.password,
+                    username: name,
+                    email: email,
+                    password: hashedPassword,
                     role: 'User'
-                }}
+                }
+            }
             const newUser = user(userData)
             // console.log(newUser);
-            newUser.save().then((result) => {
-                console.log(result)
-                res.send({ msg: 'user added successfully' })
-            }).catch(e => console.log(e))
+            newUser.save()
+                .then((result) => {
+                    console.log(result)
+                    res.send({ msg: 'user added successfully' })
+                })
+                .catch(e => console.log(e))
         }
     })
 
 })
 
 router.post('/login', (req, res) => {
-    const usrMail = req.body.email;
-    user.findOne({ email: usrMail }, (err, result) => {
+    var { email, password } = req.body;
+    user.findOne({ email: email }, (err, result) => {
         if (result === undefined) {
             console.log(`some error occured!`)
             res.status(500).send({msg: 'some error occured', error: err})
@@ -45,11 +58,19 @@ router.post('/login', (req, res) => {
         else if (result === null) {
             res.send({ msg: 'user not found', error: err })
         }
-        else if (result.email === usrMail && result.password !== req.body.password) {
+        else if (result.email === email && result.password !== password) {
             res.send({ msg: 'password incorrect', error: err })
         }
-        else if (result.email === usrMail && result.password === req.body.password) {
-            res.send({ msg: 'Logged in', user: result })
+        else if (result.email === email && result.password === password) {
+            let payload = {subject: {result}}
+            jwt.sign(payload, 'secretKey', (err, token) => {
+                if (err) {
+                    res.send({jwtError: err})
+                }
+                if (token) {
+                    res.send({ msg: 'Logged in', user: result ,token: token})
+                }
+            })
         }
     })
 })
