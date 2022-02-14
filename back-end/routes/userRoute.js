b278ec5b32c83536bdc90fd29b1567748b845466
Sarath C <sarathcs3c@gmail.com>
2022-02-14 16:30:13 +0530

    changed pw verification to through bcrypt

--- back-end/routes/userRoute.js
+++ back-end/routes/userRoute.js
@@ -4,24 +4,26 @@ const router = express.Router();
 const user = require('../models/users');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: '../config/config.env' });
+const verifyToken = require('../middlewares/authMiddleware');
 
 router.post('/newuser', (req, res) => {
     let userData = {}
-    var { name, email, password } = req.body;
+    var { username, email, password } = req.body;
 
-    user.findOne({ username: name }, (err, result) => {
+    user.findOne({ email: email }, async(err, result) => {
         if (err) {
-            res.send({msg: err.Error})
+            res.json({msg: err.Error})
         }
         else if (result) {
-            res.send({msg: 'Username taken!'})
+            res.json({msg: 'User already exists!'})
         }
         else if (!err) {
-            const salt = bcrypt.genSalt(10)
-            const hashedPassword = bcrypt.hash(password, salt)
-            if (name === 'admin') {
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(password, salt)
+            if (username === 'admin') {
                 userData = {
-                    username: name,
+                    username: username,
                     email: email,
                     password: hashedPassword,
                     role: 'Admin'
@@ -29,7 +31,7 @@ router.post('/newuser', (req, res) => {
             }
             else {
                 userData = {
-                    username: name,
+                    username: username,
                     email: email,
                     password: hashedPassword,
                     role: 'User'
@@ -39,10 +41,10 @@ router.post('/newuser', (req, res) => {
             // console.log(newUser);
             newUser.save()
                 .then((result) => {
-                    console.log(result)
-                    res.send({ msg: 'user added successfully' })
+                    console.log(`New user: ${result}`)
+                    res.json({ msg: 'Registration successfull!', user: result })
                 })
-                .catch(e => console.log(e))
+                .catch(e => console.log(`Error saving new User: ${e}`))
         }
     })
 
@@ -52,39 +54,70 @@ router.post('/login', (req, res) => {
     var { email, password } = req.body;
     user.findOne({ email: email }, (err, result) => {
         if (result === undefined) {
-            console.log(`some error occured!`)
-            res.status(500).send({msg: 'some error occured', error: err})
+            console.log(`some error occured @ router.post ==> user.findOne : result === undefined!`)
+            res.status(500).json({msg: 'Server error', error: err})
         }
         else if (result === null) {
-            res.send({ msg: 'user not found', error: err })
+            res.json({ msg: 'Email Incorrect!', error: err })
         }
-        else if (result.email === email && result.password !== password) {
-            res.send({ msg: 'password incorrect', error: err })
+        else if (result) {
+            bcrypt.compare(password, result.password)
+                .then(data => {
+                    // console.log(data)
+                    if (!data) {
+                        res.json({ msg: 'Password Incorrect!'})
+                    }
+                    let payload = {subject: result._id}
+                    jwt.sign(payload, process.env.SEC_KEY, (err, token) => {
+                        if (err) {
+                            res.json({jwtError: err})
+                        }
+                        if (token) {
+                            res.json({ msg: 'Logged in', user: {id: result._id, username: result.username, email: result.email, joined: result.joined, role: result.role} ,token: token})
+                        }
+                    })
+                })
+                .catch(e => {
+                    res.json({ msg: 'Password is Incorrect!'})
+                })
         }
-        else if (result.email === email && result.password === password) {
-            let payload = {subject: {result}}
-            jwt.sign(payload, 'secretKey', (err, token) => {
-                if (err) {
-                    res.send({jwtError: err})
-                }
-                if (token) {
-                    res.send({ msg: 'Logged in', user: result ,token: token})
-                }
-            })
-        }
+        // else if (result.email === email && result.password !== password) {
+        //     res.json({ msg: 'password incorrect', error: err })
+        // }
+        // else if (result.email === email && result.password === password) {
+        //     let payload = {subject: {result}}
+        //     jwt.sign(payload, 'secretKey', (err, token) => {
+        //         if (err) {
+        //             res.json({jwtError: err})
+        //         }
+        //         if (token) {
+        //             res.json({ msg: 'Logged in', user: result ,token: token})
+        //         }
+        //     })
+        // }
     })
 })
 
 
-router.get('/admin', (req, res) => {
-    user.find( (err, result) => {
-        if (result) {
-            res.send({ result })
-        }
-        else if (err) {
-            res.send({ err })
-        }
-    })
+router.get('/admin', verifyToken, (req, response) => {
+    user.findById(req.userId)
+        .then(res => {
+            console.log(`Logged in: ${res.role}`)
+            if (res.role === 'Admin') {
+                user.find( (err, result) => {
+                    if (result) {
+                        response.json({ result })
+                    }
+                    else if (err) {
+                        response.json({ err })
+                    }
+                })
+            }
+            else {
+                response.json({ msg: 'Unauthorized request!'})
+            }
+        })
+
 })
 
 
