e9a4ffbffbd73cc64097cbc0921e6a24e87d6ba6
Sarath C <sarathcs3c@gmail.com>
2022-02-16 17:06:24 +0530

    added delete user function in admin table

--- proj-movie/src/components/admin/Admin.js
+++ proj-movie/src/components/admin/Admin.js
@@ -8,41 +8,127 @@ import Paper from '@mui/material/Paper';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
+import { DataGrid } from '@mui/x-data-grid';
+import { Alert, Button, Snackbar } from '@mui/material';
+import { DeleteOutlineRounded } from '@material-ui/icons'
 
 
 const Admin = () => {
 
     const [users, setUsers] = useState([]);
+    const [delMsg, setDelMsg] = useState('');
+    const [isDelMsg, setIsDelMsg] = useState(false);
     let location = useLocation()
-    console.log(location)
+    // console.log(location.state)
+
+
+
+    const myrows = [
+        users.map((user) => {
+            return {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                password: user.password,
+                role: user.role,
+                joined: user.joined
+            }
+        })
+    ];
+    console.log(myrows[0])
+
+    // const createData = (username, email, password, type, date_joined) => {
+    //     return {username, email, password, type, date_joined};
+    // }
+
+    // const rows = [
+    //     users.map((user) => {
+    //         return createData(user.username, user.email,user.password, user.role, user.joined)
+    //     }),
+    // ];
+
+    const deleteHandler = (item) => {
+        console.log(item)
+        axios.post(`http://localhost:5000/register/admin/${item}`)
+            .then(res => {
+                setIsDelMsg(true)
+                setDelMsg(res.data.msg)
+                console.log(res)
+            })
+            .catch(e => console.log(e.message))
+
+    }
+
+    const renderDelButton = (params) => {
+        // console.log(params)
+        return (
+            <Button
+                variant='contained'
+                size='small'
+                color='error'
+                onClick={ () => deleteHandler(params.row.id) }
+            ><DeleteOutlineRounded /></Button>
+        )
+    }
+
+    const columns = [
+        {
+            field: 'username',
+            headerName: 'Username',
+            width: 200,
+            disableClickEventBubbling: true,
+        },
+        {
+            field: 'email',
+            headerName: 'Email',
+            width: 200,
+            disableClickEventBubbling: true,
+        },
+        {
+            field: 'password',
+            headerName: 'Password',
+            width: 500,
+            disableClickEventBubbling: true,
+        },
+        {
+            field: 'role',
+            headerName: 'Role',
+            width: 200,
+            disableClickEventBubbling: true,
+        },
+        {
+            field: 'joined',
+            headerName: 'Joined',
+            width: 200,
+            disableClickEventBubbling: true,
+        },
+        {
+            field: 'delButton',
+            headerName: 'Delete',
+            renderCell: renderDelButton,
+            // disableClickEventBubbling: true,
+
+        },
+    ];
+
 
     useEffect(() => {
         axios.get('http://localhost:5000/register/admin', { headers: { 'Authorization': `Bearer ${location.state}`}})
             .then((res) => {
                 console.log(res.data)
                 setUsers(res.data.result)
+                // console.log(users)
             }).catch(e => console.log(e))
+    }, [isDelMsg])
 
-        }, [])
-        console.log(users)
-    const createData = (username, email, password, type, date_joined) => {
-        return {username, email, password, type, date_joined};
-    }
-    const rows = [
-        users.map((user) => {
-            return createData(user.username, user.email,user.password, user.role, user.joined)
-        }),
-    //   createData('testuse2', 'email2', 'type2', 'date2'),
-    //   createData('testuser3', 'email3', 'type3', 'date3'),
-    //   createData('testuser4', 'email4', 'type4', 'date4'),
-    //   createData('testuser5', 'email5', 'type5', 'date5'),
-    ];
-    console.log(rows)
+
+
+    // console.log(rows)
     return (
         <>
             <h1>Admin page</h1>
-            <div>
-                <TableContainer style={{backgroundColor: 'darkgray'}} component={Paper}>
+            {/* <div>
+                <TableContainer  style={{backgroundColor: 'darkgray'}} component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                     <TableRow>
@@ -51,6 +137,7 @@ const Admin = () => {
                         <TableCell align="center">Password</TableCell>
                         <TableCell align="center">Account Type</TableCell>
                         <TableCell align="center">Date Joined</TableCell>
+                        <TableCell align="center">Delete</TableCell>
                     </TableRow>
                     </TableHead>
                     <TableBody>
@@ -72,6 +159,26 @@ const Admin = () => {
                 </Table>
                 </TableContainer>
 
+            </div> */}
+            <div>
+                <div style={{ height: 500, width: '100%' }}>
+                    <DataGrid
+                        style={{ backgroundColor: 'lightgray' }}
+                        rows={myrows[0]}
+                        columns={columns}
+                        // onCellClick={() => deleteHandler(myrows[0]._id)}
+                        // checkboxSelection
+                    />
+                </div>
+                {isDelMsg &&
+                    <>
+                        <Snackbar open={isDelMsg} onClose={() => { setIsDelMsg(false) }}>
+                            <Alert >
+                                {delMsg}
+                            </Alert>
+                        </Snackbar>
+                    </>
+                }
             </div>
         </>
     );
