00cd31aa827a7c6b7f1c04dfcd8ed6dd1f012da8
Sarath C <sarathcs3c@gmail.com>
2022-02-16 17:09:53 +0530

    added a route handle to delete user

--- back-end/routes/userRoute.js
+++ back-end/routes/userRoute.js
@@ -11,14 +11,16 @@ router.post('/newuser', (req, res) => {
     let userData = {}
     var { username, email, password } = req.body;
 
-    user.findOne({ email: email }, async(err, result) => {
+    user.findOne({ email: email}, async (err, userFound) => {
         if (err) {
-            res.json({msg: err.Error})
+            console.log(`err: ${err}`)
+            res.send({msg: err.Error})
         }
-        else if (result) {
-            res.json({msg: 'User already exists!'})
+        else if (userFound) {
+            res.send({msg: 'User already exists!'})
+            console.log(`userFound: ${userFound}`)
         }
-        else if (!err) {
+        else if (!userFound) {
             const salt = await bcrypt.genSalt(10)
             const hashedPassword = await bcrypt.hash(password, salt)
             if (username === 'admin') {
@@ -38,11 +40,11 @@ router.post('/newuser', (req, res) => {
                 }
             }
             const newUser = user(userData)
-            // console.log(newUser);
+            console.log(newUser);
             newUser.save()
                 .then((result) => {
                     console.log(`New user: ${result}`)
-                    res.json({ msg: 'Registration successfull!', user: result })
+                    res.send({ msg: 'Registration successfull!', user: result })
                 })
                 .catch(e => console.log(`Error saving new User: ${e}`))
         }
@@ -55,43 +57,43 @@ router.post('/login', (req, res) => {
     user.findOne({ email: email }, (err, result) => {
         if (result === undefined) {
             console.log(`some error occured @ router.post ==> user.findOne : result === undefined!`)
-            res.status(500).json({msg: 'Server error', error: err})
+            res.status(500).send({msg: 'Server error', error: err})
         }
         else if (result === null) {
-            res.json({ msg: 'Email Incorrect!', error: err })
+            res.send({ msg: 'Email Incorrect!', error: err })
         }
         else if (result) {
             bcrypt.compare(password, result.password)
                 .then(data => {
                     // console.log(data)
                     if (!data) {
-                        res.json({ msg: 'Password Incorrect!'})
+                        res.send({ msg: 'Password Incorrect!'})
                     }
                     let payload = {subject: result._id}
                     jwt.sign(payload, process.env.SEC_KEY, (err, token) => {
                         if (err) {
-                            res.json({jwtError: err})
+                            res.send({jwtError: err})
                         }
                         if (token) {
-                            res.json({ msg: 'Logged in', user: {id: result._id, username: result.username, email: result.email, joined: result.joined, role: result.role} ,token: token})
+                            res.send({ msg: 'Logged in', user: {id: result._id, username: result.username, email: result.email, joined: result.joined, role: result.role} ,token: token})
                         }
                     })
                 })
                 .catch(e => {
-                    res.json({ msg: 'Password is Incorrect!'})
+                    res.send({ msg: 'Password is Incorrect!'})
                 })
         }
         // else if (result.email === email && result.password !== password) {
-        //     res.json({ msg: 'password incorrect', error: err })
+        //     res.send({ msg: 'password incorrect', error: err })
         // }
         // else if (result.email === email && result.password === password) {
         //     let payload = {subject: {result}}
         //     jwt.sign(payload, 'secretKey', (err, token) => {
         //         if (err) {
-        //             res.json({jwtError: err})
+        //             res.send({jwtError: err})
         //         }
         //         if (token) {
-        //             res.json({ msg: 'Logged in', user: result ,token: token})
+        //             res.send({ msg: 'Logged in', user: result ,token: token})
         //         }
         //     })
         // }
@@ -106,18 +108,26 @@ router.get('/admin', verifyToken, (req, response) => {
             if (res.role === 'Admin') {
                 user.find( (err, result) => {
                     if (result) {
-                        response.json({ result })
+                        response.send({ result })
                     }
                     else if (err) {
-                        response.json({ err })
+                        response.send({ err })
                     }
                 })
             }
             else {
-                response.json({ msg: 'Unauthorized request!'})
+                response.send({ msg: 'Unauthorized request!'})
             }
         })
 
+})
+
+router.post('/admin/:id', (req, res) => {
+    console.log(req.params.id)
+    user.findByIdAndDelete(req.params.id)
+        .then(result => {
+            res.send({msg: 'User successfully deleted', result})
+        })
 })
 
 
