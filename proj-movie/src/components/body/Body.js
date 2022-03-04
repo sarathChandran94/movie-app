81826228b5a6b953ff1314310683fddd0a19fad6
Sarath C <sarathcs3c@gmail.com>
2022-03-04 13:08:15 +0530

    added useContext

--- proj-movie/src/components/body/Body.js
+++ proj-movie/src/components/body/Body.js
@@ -1,94 +1,88 @@
 import axios from "axios";
-import { createContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
-import Categories from "../categorieList/Categories";
-import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import { CircularProgress, Tooltip } from "@mui/material";
+import Categories from "../categorieList/Categories";
 import SingleMoviePage from "../singleMovie/SingleMovie";
+import { DataContext } from '../../context/Context';
 import './Body.css'
-
-
-export const Context = createContext()
 
 const Body = () => {
 
-    const [mov, setMov] = useState([])
-    const [cat, setCat] = useState([])
-    const [catSel, setCatSel] = useState('Trending')
-    const [fav, setFav] = useState([])
+    const { addFavorite } = useContext(DataContext)
+
+    const [movie, setMovie] = useState([])
+    const [category, setCategory] = useState([])
+    const [categorySelect, setCategorySelect] = useState('Trending')
+    // const [favorite, setFavorite] = useState([])
     const [isMovieClicked, setIsMovieClicked] = useState(true)
-    const [smd, setSmd] = useState(true)
-    const [favSelected, setFavSelected] = useState('Add to favourites')
-    const [favBtnClr, setFavBtnClr] = useState('warning')
-
-    useEffect(() => {
-        const getMov = () => {
-            // axios.get("https://imdb-api.com/en/API/MostPopularMovies/k_eomu4lvb").then(res => {
-            axios.get("https://api.themoviedb.org/3/trending/all/day?api_key=17e786d5aa65a489c613aaca6427cd5e")
-                .then(res => {
-                    console.log(res.data.results)
-                    setMov(res.data.results)
-                })
-                .catch(e => console.log(e))
-        }
-
-        const getCat = () => {
-            // axios.get("https://imdb-api.com/en/API/MostPopularMovies/k_eomu4lvb").then(res => {
-            axios.get("https://api.themoviedb.org/3/genre/movie/list?api_key=17e786d5aa65a489c613aaca6427cd5e")
-                .then(res => {
-                    // console.log(res.data)
-                    setCat(res.data.genres)
-                })
-                .catch(e => console.log(e))
-        }
-        getMov()
-        getCat()
-    }, []);
+    const [singleMovie, setSingleMovie] = useState(true)
+    // const [favoriteSelected, setFavoriteSelected] = useState('Add to favourites')
+    // const [favBtnClr, setFavBtnClr] = useState('warning')
 
 
-    const handleClick = (item) => {
-        setFav([...fav, item])
-        console.log(fav)
-        setFavBtnClr('success')
-        // storing to local storage
-        localStorage.setItem('fMov', JSON.stringify(fav))
+    const getMovies = () => {
+        // axios.get("https://imdb-api.com/en/API/MostPopularMovies/k_eomu4lvb").then(res => {
+        axios.get("https://api.themoviedb.org/3/trending/movie/day?api_key=17e786d5aa65a489c613aaca6427cd5e&type=movies")
+            .then(res => {
+                // console.log(res.data.results)
+                setMovie(res.data.results)
+            })
+            .catch(e => console.log(e))
     }
 
+
+
+    const getCategories = () => {
+        // axios.get("https://imdb-api.com/en/API/MostPopularMovies/k_eomu4lvb").then(res => {
+        axios.get("https://api.themoviedb.org/3/genre/movie/list?api_key=17e786d5aa65a489c613aaca6427cd5e")
+            .then(res => {
+                // console.log(res.data)
+                setCategory(res.data.genres)
+            })
+            .catch(e => console.log(e))
+    }
 
 
     const movieClicked = (item) => {
-        setSmd(item)
+        setSingleMovie(item)
         setIsMovieClicked(false)
     }
+
+    useEffect(() => {
+        getMovies()
+        getCategories()
+    },[])
+
     return (
-        <Context.Provider value={{Data:[fav,setFav]}}>
+        <>
             <div>
                 {/* <div className='background'>
                 </div> */}
                 <div className='foregorund'>
 
                 <Container className="heading" fluid>
-                    <h1>{ catSel }</h1>
+                    <h1>{ categorySelect }</h1>
                 </Container>
                 <Row className="backdrop">
                     {isMovieClicked ?
                         <>
                             <Col className="p-3" sm={3} >
-                                <Categories cat={cat} setCat={setCat} mov={mov} setMov={setMov} setCatSel={setCatSel} />
+                                <Categories cat={category} setCat={setCategory} mov={movie} setMov={setMovie} setCatSel={setCategorySelect} />
                             </Col>
                             <Col className="p-3" sm={9} >
                                 <Row xs={1} sm={2} md={4} >
                                     {
-                                        mov.map((v, i) => {
+                                        movie.map((v, i) => {
                                             return (
                                                 <Col key={i}>
                                                     <Card key={i} style={{height: '450px'}} className="movieCard m-3">
                                                         <Card.Img onClick={() => movieClicked(v.id)} style={{height: '300px'}} variant="top" src={`https://image.tmdb.org/t/p/w300/${v.poster_path}`} />
                                                         <Row>
                                                             <Col className='m-2'>
-                                                                <Tooltip title={ favSelected } placement="top">
-                                                                    <Card.Text><Button variant={ favBtnClr } size="sm" onClick={() => handleClick(v)} value={JSON.stringify(v)} ><ThumbUpIcon  /></Button></Card.Text>
-                                                            </Tooltip>
+                                                                <Tooltip title='Add to favorites' placement="top">
+                                                                <Button variant='warning' size="sm" onClick={(e) => addFavorite(e,v)} >Add to favorite</Button>
+                                                                </Tooltip>
                                                             </Col>
                                                             <Col className='m-2'>
                                                                 <CircularProgress variant="determinate" value={v.vote_average * 10} />
@@ -106,12 +100,12 @@ const Body = () => {
                                 </Row>
                             </Col>
                         </>
-                        :   <SingleMoviePage value={smd} />
+                        :   <SingleMoviePage value={singleMovie} />
                     }
                 </Row>
                 </div>
             </div>
-        </Context.Provider>
+        </>
     );
 }
 
