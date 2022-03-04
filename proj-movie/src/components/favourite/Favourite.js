e86741b39c5f09a23e6a00b4c29d3c3a8234f48a
Sarath C <sarathcs3c@gmail.com>
2022-03-04 13:05:07 +0530

    added useReducer for implementing favorite section

--- proj-movie/src/components/favourite/Favourite.js
+++ proj-movie/src/components/favourite/Favourite.js
@@ -1,82 +1,66 @@
+import { useState, useEffect, useContext } from "react"
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { useState,useEffect } from "react"
-
-import '../body/Body.css'
 import { Button, Container } from 'react-bootstrap';
 import { CircularProgress } from '@material-ui/core';
 import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
+import '../body/Body.css'
+import { DataContext } from '../../context/Context'
 
 
 const Favourite = () => {
 
+    const [movieList, setMovieList] = useState([])
 
-    const [fav, setFav] = useState([])
+    const { favoritesList, removeFavorite } = useContext(DataContext)
+
     useEffect(() => {
-        setFav(JSON.parse(localStorage.getItem('fMov')))
-    },[])
-    console.log(fav)
-    const handleClick = (id) => {
-        const filteredItems = fav.filter((v, i) => {
-            return i !== id
-        })
-        setFav(filteredItems)
-    }
+        setMovieList(favoritesList)
+    }, [favoritesList]);
+    console.log(movieList)
+
+
+    // useEffect(() => {
+    //     setFav(JSON.parse(localStorage.getItem('fMov')))
+    // },[])
+    // console.log(fav)
+    // const handleClick = (id) => {
+    //     const filteredItems = movieList.filter((v, i) => {
+    //         return i !== id
+    //     })
+    //     setMovieList(filteredItems)
+    // }
     return (
         <>
             <div>
                 <div className='foreground'>
                     <Container className='heading'>
-                        <h1 >Favourites</h1>
+                        <h1 >Favorites</h1>
                     </Container>
-                    {fav.length === 0  ? <h3 style={{color: 'grey'}}>No favourites yet...</h3> :
+                    {movieList.length === 0  ? <h3 style={{color: 'grey'}}>No favorites yet...</h3> :
                     <Row xs={1} sm={2} md={6} >
                     {
-                        fav.map((v, i) => {
+                        movieList.map((v, i) => {
                             return (
                                 <>
-                                    <Col key={v[0].id} >
-                                        <Card style={{height: '450px'}} className="movieCard">
-                                            <Card.Img style={{height: '300px'}} variant="top" src={`https://image.tmdb.org/t/p/w300/${v[0].poster_path}`} />
+                                    <Col key={i} >
+                                        <Card key={i} style={{height: '450px'}} className="movieCard">
+                                            <Card.Img style={{height: '300px'}} variant="top" src={`https://image.tmdb.org/t/p/w300/${v.poster_path}`} />
                                             <Row>
                                                 <Col className='m-2'>
-                                                    <Button variant='danger' size='sm' onClick={() => handleClick(i)}><span><RemoveCircleOutlineIcon /></span>Remove</Button>
+                                                    <Button variant='danger' size='sm' onClick={() => removeFavorite(i)}><span><RemoveCircleOutlineIcon /></span>Remove</Button>
                                                 </Col>
                                                 <Col className='m-2'>
-                                                    <CircularProgress variant="determinate" value={v[0].vote_average * 10} />
-                                                    <Card.Text>{v[0].vote_average}</Card.Text>
+                                                    <CircularProgress variant="determinate" value={v.vote_average * 10} />
+                                                    <Card.Text>{v.vote_average}</Card.Text>
                                                 </Col>
                                             </Row>
                                             <Card.Body>
-                                                <Card.Title>{v[0].title || v[0].name}</Card.Title>
+                                                <Card.Title>{v.title || v.name}</Card.Title>
                                             </Card.Body>
                                         </Card>
                                     </Col>
-                                    {/* <Col >
-                                        <Card key={i} style={{height: '450px',width: '150px'}} className="movieCard shadow shadow-sm">
-                                            <Card.Img style={{height: '300px'}} variant="top" src={`https://image.tmdb.org/t/p/w300/${v[0].poster_path}`} />
-                                            <Row>
-                                                <Col>
-                                                    <Card.Text></Card.Text>
-                                                </Col>
-                                                <Col>
-                                                    <Card.Text>Rating:{v[0].vote_average}</Card.Text>
-                                                </Col>
-                                            </Row>
-                                            <Card.Body>
-                                                <Card.Title>{v[0].title || v[0].name}</Card.Title>
-                                                <Row>
-                                                    <Col>
-                                                        <Card.Text>{v[0].release_date }</Card.Text>
-                                                    </Col>
-                                                    <Col>
-                                                        <Card.Text>Genre:</Card.Text>
-                                                    </Col>
-                                                </Row>
-                                            </Card.Body>
-                                        </Card>
-                                    </Col> */}
                                 </>
                             )
                         })
